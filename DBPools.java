a92c01c681b4cd9ac4066421257022033d0d5c7a
jimmywen54 <wenzm54@foxmail.com>
2016-06-13 15:36:55 +0800

    Update DBPools.java

--- DBPools.java
+++ DBPools.java
@@ -1,13 +1,3 @@
-/*
- * Copyright (c) 2015 yy.com. 
- *
- * All Rights Reserved.
- *
- * This program is the confidential and proprietary information of 
- * YY.INC. ("Confidential Information").  You shall not disclose such
- * Confidential Information and shall use it only in accordance with
- * the terms of the license agreement you entered into with yy.com.
- */
 package vicody.pool.db;
 
 
